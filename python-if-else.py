807c566e63bdbe6aad4bd4ee1b246045b716fb2a
AbouLkhair <husam.tariq@outlook.com>
2021-08-11 11:41:40 +0300

    Update python-if-else.py

--- python-if-else.py
+++ python-if-else.py
@@ -1,4 +1,3 @@
 n = int(input().strip())
 check = {True:"Not Weird" , False: "Weird"}
-
-print(check[n % 2 == 0 and  n in range(6,20) or n > 20 ])
+print(check[n % 2 == 0 and  (n in range(2,5) or n > 20) ])
